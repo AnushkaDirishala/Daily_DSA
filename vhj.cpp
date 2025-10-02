af8b96076eb49d3c8f38bef6d41289e236129c6f
AnushkaDirishala <1861048anushka@gmail.com>
2025-10-02 10:42:45 +0530

    these is new

---
+++ vhj.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hey Charan!\n";
+    cout<<"Sometimes I feel like ";
+    cout<<"I feel like u love me\n";
+    cout<<"Do you love me....?\n";
+    cout<<"I will be waiting for your reply";
+    return 0;
+}
