83266d24269280a056ccff7395e4ad6a5e451612
AnushkaDirishala <1861048anushka@gmail.com>
2025-10-02 10:45:58 +0530

    i removed one line

--- vhj.cpp
+++ vhj.cpp
@@ -4,7 +4,7 @@ int main(){
     cout<<"Hey Charan!\n";
     cout<<"Sometimes I feel like ";
     cout<<"I feel like u love me\n";
-    cout<<"Do you love me....?\n";
+    cout<<"Don";
     cout<<"I will be waiting for your reply";
     return 0;
 }
