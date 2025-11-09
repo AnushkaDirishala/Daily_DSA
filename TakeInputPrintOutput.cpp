f9d33acf4f4950dfc37f1df268905c6e659fb907
AnushkaDirishala <1861048anushka@gmail.com>
2025-11-09 12:12:52 +0530

    takeinputandprintoutput

---
+++ TakeInputPrintOutput.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+//write a program that takes input of age
+//and prints if you are an adult or not
+int main(){
+    int age;
+    cin>>age;
+    if(age>=18){
+      cout<<" you are an adult";
+    }
+    else{
+      cout<<"You are not an adult";
+    }
+      return 0;
+    }
