4821e3a8999c83616baf9bf96a2dbafa871e0cbd
AnushkaDirishala <1861048anushka@gmail.com>
2025-11-09 11:38:47 +0530

    character

--- using string datatype.cpp
+++ using string datatype.cpp
@@ -6,3 +6,12 @@ int main(){
     cout<<str;
     return 0;
 }
+
+//  git commit -m " "
+
+//  -m means message u shoud write something in that quotes , it means later u will what code u wrote 
+// arey dont give file names with space its nota good practice 
+// allways give file names in camelcase 
+// like eg
+// TakingInput.cpp --> this is good way 
+// taking input.cpp --> thisis worst it showd that u dont know anyting about laptop and ur a bad software engineer
